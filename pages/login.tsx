d8d7460381bc08ca5837c9428c3eeeb55be71664
unknown <pt.chanrit@gmail.com>
2024-08-23 15:45:26 +0700

    first commit

--- pages/login.tsx
+++ pages/login.tsx
@@ -75,7 +75,7 @@ const Login: React.FC = () => {
         </form>
         <div className="mt-6 text-center">
           <p className="text-sm text-gray-600">
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <Link
               href="/register"
               className="font-medium text-blue-600 hover:text-blue-500"
